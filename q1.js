62d878399e7574a2b5e5cd1b12b7280cef670501
Nir Fainshtein <fainir2006@gmail.com>
2021-01-19 08:42:11 +0200

    first commit

---
+++ q1.js
@@ -0,0 +1,11 @@
+function run() {
+    const input = document.getElementById("input").value;
+    const output = document.getElementById("output");
+    let out = "";
+/**/
+    // write your code here
+    // out = input + 5;
+
+/**/
+    output.innerText = out;
+}
