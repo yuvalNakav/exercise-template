7186199613c27d26f9d0162e02126b29682ab670
yuvalNakav <yuvalnakav1@gmail.com>
2021-01-20 22:24:47 +0200

    q6

--- q6.js
+++ q6.js
@@ -1,8 +1,8 @@
-const number = 456485312264;
+const number = 456845648465;
 const str = number.toString();
 const result = [str[0]];
-for (let i = 0; i < str.length; i++){
-    if (str[i]%2 === 0 && str[i-1]%2 === 0) {
+for (let i = 1; i < str.length; i++){
+    if (str[i] % 2 === 0 && str[i+1] % 2 === 0) {
         result.push("-" + str[i]);
     } else {
         result.push(str[i]);
