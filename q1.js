39e8644f02f8a9ac3931d9836aefef9067f4aaff
yuvalNakav <yuvalnakav1@gmail.com>
2021-01-20 12:15:45 +0200

    update q1

--- q1.js
+++ q1.js
@@ -1,12 +1,11 @@
-let is_array = function (input) 
-    if (toString.call(input) === "[object Array]") {
+function is_array(a) {
+  if (toString.call(a) === "[object Array]") {
     return true;
     } else {
         return false;   
     };
-  console.log(is_array('w3resource'));
-  console.log(is_array([1, 2, 4, 0]));
-  
+}
+    
 
 console.log(is_array('w3resource'));
 console.log(is_array([1, 2, 4, 0]));
